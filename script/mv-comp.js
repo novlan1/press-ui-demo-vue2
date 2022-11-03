471ba7e0e5ce5106216485248b250b2e3557b3fd
novlan1 <1576271227@qq.com>
2022-11-03 19:03:40 +0800

    feat: 增加eslint

--- script/mv-comp.js
+++ script/mv-comp.js
@@ -1,54 +1,50 @@
-const fs = require('fs')
-const path = require('path')
-const SAVE_FILE_PATH = path.resolve(__dirname, '../src/global-comp.js')
+const fs = require('fs');
+const path = require('path');
+const SAVE_FILE_PATH = path.resolve(__dirname, '../src/global-comp.js');
 
-const EXCLUDE_COMPONENT_LIST = ['UniListAd']
+const EXCLUDE_COMPONENT_LIST = ['UniListAd'];
 
 function uppercase(name) {
-  return name.replace(/(^|-)\S/g, (a,b)=>{
-    return a.toUpperCase()
-  }).replace(/-/g, '')
+  return name.replace(/(^|-)\S/g, a => a.toUpperCase()).replace(/-/g, '');
 }
 
 function main() {
-  const dir = './src/uni_modules/'
-  const dirs = fs.readdirSync(dir)
+  const dir = './src/uni_modules/';
+  const dirs = fs.readdirSync(dir);
 
-  const compNames = []
+  const compNames = [];
 
-  const strList = dirs.map(dir => {
-    const compsPath = `./src/uni_modules/${dir}/components`
-    
+  const strList = dirs.map((dir) => {
+    const compsPath = `./src/uni_modules/${dir}/components`;
+
     // components文件夹存在
     if (fs.existsSync(compsPath)) {
-      const comps = fs.readdirSync(compsPath)
+      const comps = fs.readdirSync(compsPath);
 
-       return comps.map(comp => {
-         const compPath = `${compsPath}/${comp}/${comp}.vue`
+      return comps.map((comp) => {
+        const compPath = `${compsPath}/${comp}/${comp}.vue`;
 
         // 组件按照以下规范： 组件名/组件名.vue
         if (fs.existsSync(compPath)) {
-          const compName = uppercase(comp)
-          
+          const compName = uppercase(comp);
+
           if (EXCLUDE_COMPONENT_LIST.indexOf(compName) === -1) {
-            console.log('正在处理: ', compPath)
-            compNames.push(compName)
-            return  `import ${compName} from './uni_modules/${dir}/components/${comp}/${comp}.vue'`
+            console.log('正在处理: ', compPath);
+            compNames.push(compName);
+            return  `import ${compName} from './uni_modules/${dir}/components/${comp}/${comp}.vue'`;
           }
         }
-      })
+      });
     }
   })
-  .flat()
-  .concat(compNames.map(name => {
-    return `Vue.component('${name}',${name})`
-  }))
+    .flat()
+    .concat(compNames.map(name => `Vue.component('${name}',${name})`));
 
-  const content = ['import Vue from \'vue\''].concat(strList).join('\n')
+  const content = ['import Vue from \'vue\''].concat(strList).join('\n');
 
   fs.writeFileSync(SAVE_FILE_PATH, content, {
-    encoding: 'utf-8'
-  })
+    encoding: 'utf-8',
+  });
 }
 
-main()
+main();
--- src/global-comp.js
+++ src/global-comp.js
@@ -1,131 +1,131 @@
-import Vue from 'vue'
-import MatchLoading from './uni_modules/match/components/match-loading/match-loading.vue'
-import UniBadge from './uni_modules/uni-badge/components/uni-badge/uni-badge.vue'
-import UniBreadcrumb from './uni_modules/uni-breadcrumb/components/uni-breadcrumb/uni-breadcrumb.vue'
-import UniBreadcrumbItem from './uni_modules/uni-breadcrumb/components/uni-breadcrumb-item/uni-breadcrumb-item.vue'
-import UniCalendar from './uni_modules/uni-calendar/components/uni-calendar/uni-calendar.vue'
-import UniCard from './uni_modules/uni-card/components/uni-card/uni-card.vue'
-import UniCollapse from './uni_modules/uni-collapse/components/uni-collapse/uni-collapse.vue'
-import UniCollapseItem from './uni_modules/uni-collapse/components/uni-collapse-item/uni-collapse-item.vue'
-import UniCombox from './uni_modules/uni-combox/components/uni-combox/uni-combox.vue'
-import UniCountdown from './uni_modules/uni-countdown/components/uni-countdown/uni-countdown.vue'
-import UniDataCheckbox from './uni_modules/uni-data-checkbox/components/uni-data-checkbox/uni-data-checkbox.vue'
-import UniDataPicker from './uni_modules/uni-data-picker/components/uni-data-picker/uni-data-picker.vue'
-import UniDataPickerview from './uni_modules/uni-data-picker/components/uni-data-pickerview/uni-data-pickerview.vue'
-import UniDataSelect from './uni_modules/uni-data-select/components/uni-data-select/uni-data-select.vue'
-import UniDateformat from './uni_modules/uni-dateformat/components/uni-dateformat/uni-dateformat.vue'
-import UniDatetimePicker from './uni_modules/uni-datetime-picker/components/uni-datetime-picker/uni-datetime-picker.vue'
-import UniDrawer from './uni_modules/uni-drawer/components/uni-drawer/uni-drawer.vue'
-import UniEasyinput from './uni_modules/uni-easyinput/components/uni-easyinput/uni-easyinput.vue'
-import UniFab from './uni_modules/uni-fab/components/uni-fab/uni-fab.vue'
-import UniFav from './uni_modules/uni-fav/components/uni-fav/uni-fav.vue'
-import UniFilePicker from './uni_modules/uni-file-picker/components/uni-file-picker/uni-file-picker.vue'
-import UniForms from './uni_modules/uni-forms/components/uni-forms/uni-forms.vue'
-import UniFormsItem from './uni_modules/uni-forms/components/uni-forms-item/uni-forms-item.vue'
-import UniGoodsNav from './uni_modules/uni-goods-nav/components/uni-goods-nav/uni-goods-nav.vue'
-import UniGrid from './uni_modules/uni-grid/components/uni-grid/uni-grid.vue'
-import UniGridItem from './uni_modules/uni-grid/components/uni-grid-item/uni-grid-item.vue'
-import UniGroup from './uni_modules/uni-group/components/uni-group/uni-group.vue'
-import UniIcons from './uni_modules/uni-icons/components/uni-icons/uni-icons.vue'
-import UniIndexedList from './uni_modules/uni-indexed-list/components/uni-indexed-list/uni-indexed-list.vue'
-import UniLink from './uni_modules/uni-link/components/uni-link/uni-link.vue'
-import UniList from './uni_modules/uni-list/components/uni-list/uni-list.vue'
+import Vue from 'vue';
+import MatchLoading from './uni_modules/match/components/match-loading/match-loading.vue';
+import UniBadge from './uni_modules/uni-badge/components/uni-badge/uni-badge.vue';
+import UniBreadcrumb from './uni_modules/uni-breadcrumb/components/uni-breadcrumb/uni-breadcrumb.vue';
+import UniBreadcrumbItem from './uni_modules/uni-breadcrumb/components/uni-breadcrumb-item/uni-breadcrumb-item.vue';
+import UniCalendar from './uni_modules/uni-calendar/components/uni-calendar/uni-calendar.vue';
+import UniCard from './uni_modules/uni-card/components/uni-card/uni-card.vue';
+import UniCollapse from './uni_modules/uni-collapse/components/uni-collapse/uni-collapse.vue';
+import UniCollapseItem from './uni_modules/uni-collapse/components/uni-collapse-item/uni-collapse-item.vue';
+import UniCombox from './uni_modules/uni-combox/components/uni-combox/uni-combox.vue';
+import UniCountdown from './uni_modules/uni-countdown/components/uni-countdown/uni-countdown.vue';
+import UniDataCheckbox from './uni_modules/uni-data-checkbox/components/uni-data-checkbox/uni-data-checkbox.vue';
+import UniDataPicker from './uni_modules/uni-data-picker/components/uni-data-picker/uni-data-picker.vue';
+import UniDataPickerview from './uni_modules/uni-data-picker/components/uni-data-pickerview/uni-data-pickerview.vue';
+import UniDataSelect from './uni_modules/uni-data-select/components/uni-data-select/uni-data-select.vue';
+import UniDateformat from './uni_modules/uni-dateformat/components/uni-dateformat/uni-dateformat.vue';
+import UniDatetimePicker from './uni_modules/uni-datetime-picker/components/uni-datetime-picker/uni-datetime-picker.vue';
+import UniDrawer from './uni_modules/uni-drawer/components/uni-drawer/uni-drawer.vue';
+import UniEasyinput from './uni_modules/uni-easyinput/components/uni-easyinput/uni-easyinput.vue';
+import UniFab from './uni_modules/uni-fab/components/uni-fab/uni-fab.vue';
+import UniFav from './uni_modules/uni-fav/components/uni-fav/uni-fav.vue';
+import UniFilePicker from './uni_modules/uni-file-picker/components/uni-file-picker/uni-file-picker.vue';
+import UniForms from './uni_modules/uni-forms/components/uni-forms/uni-forms.vue';
+import UniFormsItem from './uni_modules/uni-forms/components/uni-forms-item/uni-forms-item.vue';
+import UniGoodsNav from './uni_modules/uni-goods-nav/components/uni-goods-nav/uni-goods-nav.vue';
+import UniGrid from './uni_modules/uni-grid/components/uni-grid/uni-grid.vue';
+import UniGridItem from './uni_modules/uni-grid/components/uni-grid-item/uni-grid-item.vue';
+import UniGroup from './uni_modules/uni-group/components/uni-group/uni-group.vue';
+import UniIcons from './uni_modules/uni-icons/components/uni-icons/uni-icons.vue';
+import UniIndexedList from './uni_modules/uni-indexed-list/components/uni-indexed-list/uni-indexed-list.vue';
+import UniLink from './uni_modules/uni-link/components/uni-link/uni-link.vue';
+import UniList from './uni_modules/uni-list/components/uni-list/uni-list.vue';
 
-import UniListChat from './uni_modules/uni-list/components/uni-list-chat/uni-list-chat.vue'
-import UniListItem from './uni_modules/uni-list/components/uni-list-item/uni-list-item.vue'
-import UniLoadMore from './uni_modules/uni-load-more/components/uni-load-more/uni-load-more.vue'
-import UniNavBar from './uni_modules/uni-nav-bar/components/uni-nav-bar/uni-nav-bar.vue'
-import UniNoticeBar from './uni_modules/uni-notice-bar/components/uni-notice-bar/uni-notice-bar.vue'
-import UniNumberBox from './uni_modules/uni-number-box/components/uni-number-box/uni-number-box.vue'
-import UniPagination from './uni_modules/uni-pagination/components/uni-pagination/uni-pagination.vue'
-import UniPopup from './uni_modules/uni-popup/components/uni-popup/uni-popup.vue'
-import UniPopupDialog from './uni_modules/uni-popup/components/uni-popup-dialog/uni-popup-dialog.vue'
-import UniPopupMessage from './uni_modules/uni-popup/components/uni-popup-message/uni-popup-message.vue'
-import UniPopupShare from './uni_modules/uni-popup/components/uni-popup-share/uni-popup-share.vue'
-import UniRate from './uni_modules/uni-rate/components/uni-rate/uni-rate.vue'
-import UniCol from './uni_modules/uni-row/components/uni-col/uni-col.vue'
-import UniRow from './uni_modules/uni-row/components/uni-row/uni-row.vue'
+import UniListChat from './uni_modules/uni-list/components/uni-list-chat/uni-list-chat.vue';
+import UniListItem from './uni_modules/uni-list/components/uni-list-item/uni-list-item.vue';
+import UniLoadMore from './uni_modules/uni-load-more/components/uni-load-more/uni-load-more.vue';
+import UniNavBar from './uni_modules/uni-nav-bar/components/uni-nav-bar/uni-nav-bar.vue';
+import UniNoticeBar from './uni_modules/uni-notice-bar/components/uni-notice-bar/uni-notice-bar.vue';
+import UniNumberBox from './uni_modules/uni-number-box/components/uni-number-box/uni-number-box.vue';
+import UniPagination from './uni_modules/uni-pagination/components/uni-pagination/uni-pagination.vue';
+import UniPopup from './uni_modules/uni-popup/components/uni-popup/uni-popup.vue';
+import UniPopupDialog from './uni_modules/uni-popup/components/uni-popup-dialog/uni-popup-dialog.vue';
+import UniPopupMessage from './uni_modules/uni-popup/components/uni-popup-message/uni-popup-message.vue';
+import UniPopupShare from './uni_modules/uni-popup/components/uni-popup-share/uni-popup-share.vue';
+import UniRate from './uni_modules/uni-rate/components/uni-rate/uni-rate.vue';
+import UniCol from './uni_modules/uni-row/components/uni-col/uni-col.vue';
+import UniRow from './uni_modules/uni-row/components/uni-row/uni-row.vue';
 
-import UniSearchBar from './uni_modules/uni-search-bar/components/uni-search-bar/uni-search-bar.vue'
-import UniSection from './uni_modules/uni-section/components/uni-section/uni-section.vue'
-import UniSegmentedControl from './uni_modules/uni-segmented-control/components/uni-segmented-control/uni-segmented-control.vue'
-import UniSteps from './uni_modules/uni-steps/components/uni-steps/uni-steps.vue'
-import UniSwipeAction from './uni_modules/uni-swipe-action/components/uni-swipe-action/uni-swipe-action.vue'
-import UniSwipeActionItem from './uni_modules/uni-swipe-action/components/uni-swipe-action-item/uni-swipe-action-item.vue'
-import UniSwiperDot from './uni_modules/uni-swiper-dot/components/uni-swiper-dot/uni-swiper-dot.vue'
-import UniTable from './uni_modules/uni-table/components/uni-table/uni-table.vue'
-import UniTbody from './uni_modules/uni-table/components/uni-tbody/uni-tbody.vue'
-import UniTd from './uni_modules/uni-table/components/uni-td/uni-td.vue'
-import UniTh from './uni_modules/uni-table/components/uni-th/uni-th.vue'
-import UniThead from './uni_modules/uni-table/components/uni-thead/uni-thead.vue'
-import UniTr from './uni_modules/uni-table/components/uni-tr/uni-tr.vue'
-import UniTag from './uni_modules/uni-tag/components/uni-tag/uni-tag.vue'
-import UniTest from './uni_modules/uni-test/components/uni-test/uni-test.vue'
-import UniTitle from './uni_modules/uni-title/components/uni-title/uni-title.vue'
-import UniTooltip from './uni_modules/uni-tooltip/components/uni-tooltip/uni-tooltip.vue'
-import UniTransition from './uni_modules/uni-transition/components/uni-transition/uni-transition.vue'
-import UniUi from './uni_modules/uni-ui/components/uni-ui/uni-ui.vue'
-Vue.component('MatchLoading',MatchLoading)
-Vue.component('UniBadge',UniBadge)
-Vue.component('UniBreadcrumb',UniBreadcrumb)
-Vue.component('UniBreadcrumbItem',UniBreadcrumbItem)
-Vue.component('UniCalendar',UniCalendar)
-Vue.component('UniCard',UniCard)
-Vue.component('UniCollapse',UniCollapse)
-Vue.component('UniCollapseItem',UniCollapseItem)
-Vue.component('UniCombox',UniCombox)
-Vue.component('UniCountdown',UniCountdown)
-Vue.component('UniDataCheckbox',UniDataCheckbox)
-Vue.component('UniDataPicker',UniDataPicker)
-Vue.component('UniDataPickerview',UniDataPickerview)
-Vue.component('UniDataSelect',UniDataSelect)
-Vue.component('UniDateformat',UniDateformat)
-Vue.component('UniDatetimePicker',UniDatetimePicker)
-Vue.component('UniDrawer',UniDrawer)
-Vue.component('UniEasyinput',UniEasyinput)
-Vue.component('UniFab',UniFab)
-Vue.component('UniFav',UniFav)
-Vue.component('UniFilePicker',UniFilePicker)
-Vue.component('UniForms',UniForms)
-Vue.component('UniFormsItem',UniFormsItem)
-Vue.component('UniGoodsNav',UniGoodsNav)
-Vue.component('UniGrid',UniGrid)
-Vue.component('UniGridItem',UniGridItem)
-Vue.component('UniGroup',UniGroup)
-Vue.component('UniIcons',UniIcons)
-Vue.component('UniIndexedList',UniIndexedList)
-Vue.component('UniLink',UniLink)
-Vue.component('UniList',UniList)
-Vue.component('UniListChat',UniListChat)
-Vue.component('UniListItem',UniListItem)
-Vue.component('UniLoadMore',UniLoadMore)
-Vue.component('UniNavBar',UniNavBar)
-Vue.component('UniNoticeBar',UniNoticeBar)
-Vue.component('UniNumberBox',UniNumberBox)
-Vue.component('UniPagination',UniPagination)
-Vue.component('UniPopup',UniPopup)
-Vue.component('UniPopupDialog',UniPopupDialog)
-Vue.component('UniPopupMessage',UniPopupMessage)
-Vue.component('UniPopupShare',UniPopupShare)
-Vue.component('UniRate',UniRate)
-Vue.component('UniCol',UniCol)
-Vue.component('UniRow',UniRow)
-Vue.component('UniSearchBar',UniSearchBar)
-Vue.component('UniSection',UniSection)
-Vue.component('UniSegmentedControl',UniSegmentedControl)
-Vue.component('UniSteps',UniSteps)
-Vue.component('UniSwipeAction',UniSwipeAction)
-Vue.component('UniSwipeActionItem',UniSwipeActionItem)
-Vue.component('UniSwiperDot',UniSwiperDot)
-Vue.component('UniTable',UniTable)
-Vue.component('UniTbody',UniTbody)
-Vue.component('UniTd',UniTd)
-Vue.component('UniTh',UniTh)
-Vue.component('UniThead',UniThead)
-Vue.component('UniTr',UniTr)
-Vue.component('UniTag',UniTag)
-Vue.component('UniTest',UniTest)
-Vue.component('UniTitle',UniTitle)
-Vue.component('UniTooltip',UniTooltip)
-Vue.component('UniTransition',UniTransition)
-Vue.component('UniUi',UniUi)
+import UniSearchBar from './uni_modules/uni-search-bar/components/uni-search-bar/uni-search-bar.vue';
+import UniSection from './uni_modules/uni-section/components/uni-section/uni-section.vue';
+import UniSegmentedControl from './uni_modules/uni-segmented-control/components/uni-segmented-control/uni-segmented-control.vue';
+import UniSteps from './uni_modules/uni-steps/components/uni-steps/uni-steps.vue';
+import UniSwipeAction from './uni_modules/uni-swipe-action/components/uni-swipe-action/uni-swipe-action.vue';
+import UniSwipeActionItem from './uni_modules/uni-swipe-action/components/uni-swipe-action-item/uni-swipe-action-item.vue';
+import UniSwiperDot from './uni_modules/uni-swiper-dot/components/uni-swiper-dot/uni-swiper-dot.vue';
+import UniTable from './uni_modules/uni-table/components/uni-table/uni-table.vue';
+import UniTbody from './uni_modules/uni-table/components/uni-tbody/uni-tbody.vue';
+import UniTd from './uni_modules/uni-table/components/uni-td/uni-td.vue';
+import UniTh from './uni_modules/uni-table/components/uni-th/uni-th.vue';
+import UniThead from './uni_modules/uni-table/components/uni-thead/uni-thead.vue';
+import UniTr from './uni_modules/uni-table/components/uni-tr/uni-tr.vue';
+import UniTag from './uni_modules/uni-tag/components/uni-tag/uni-tag.vue';
+import UniTest from './uni_modules/uni-test/components/uni-test/uni-test.vue';
+import UniTitle from './uni_modules/uni-title/components/uni-title/uni-title.vue';
+import UniTooltip from './uni_modules/uni-tooltip/components/uni-tooltip/uni-tooltip.vue';
+import UniTransition from './uni_modules/uni-transition/components/uni-transition/uni-transition.vue';
+import UniUi from './uni_modules/uni-ui/components/uni-ui/uni-ui.vue';
+Vue.component('MatchLoading', MatchLoading);
+Vue.component('UniBadge', UniBadge);
+Vue.component('UniBreadcrumb', UniBreadcrumb);
+Vue.component('UniBreadcrumbItem', UniBreadcrumbItem);
+Vue.component('UniCalendar', UniCalendar);
+Vue.component('UniCard', UniCard);
+Vue.component('UniCollapse', UniCollapse);
+Vue.component('UniCollapseItem', UniCollapseItem);
+Vue.component('UniCombox', UniCombox);
+Vue.component('UniCountdown', UniCountdown);
+Vue.component('UniDataCheckbox', UniDataCheckbox);
+Vue.component('UniDataPicker', UniDataPicker);
+Vue.component('UniDataPickerview', UniDataPickerview);
+Vue.component('UniDataSelect', UniDataSelect);
+Vue.component('UniDateformat', UniDateformat);
+Vue.component('UniDatetimePicker', UniDatetimePicker);
+Vue.component('UniDrawer', UniDrawer);
+Vue.component('UniEasyinput', UniEasyinput);
+Vue.component('UniFab', UniFab);
+Vue.component('UniFav', UniFav);
+Vue.component('UniFilePicker', UniFilePicker);
+Vue.component('UniForms', UniForms);
+Vue.component('UniFormsItem', UniFormsItem);
+Vue.component('UniGoodsNav', UniGoodsNav);
+Vue.component('UniGrid', UniGrid);
+Vue.component('UniGridItem', UniGridItem);
+Vue.component('UniGroup', UniGroup);
+Vue.component('UniIcons', UniIcons);
+Vue.component('UniIndexedList', UniIndexedList);
+Vue.component('UniLink', UniLink);
+Vue.component('UniList', UniList);
+Vue.component('UniListChat', UniListChat);
+Vue.component('UniListItem', UniListItem);
+Vue.component('UniLoadMore', UniLoadMore);
+Vue.component('UniNavBar', UniNavBar);
+Vue.component('UniNoticeBar', UniNoticeBar);
+Vue.component('UniNumberBox', UniNumberBox);
+Vue.component('UniPagination', UniPagination);
+Vue.component('UniPopup', UniPopup);
+Vue.component('UniPopupDialog', UniPopupDialog);
+Vue.component('UniPopupMessage', UniPopupMessage);
+Vue.component('UniPopupShare', UniPopupShare);
+Vue.component('UniRate', UniRate);
+Vue.component('UniCol', UniCol);
+Vue.component('UniRow', UniRow);
+Vue.component('UniSearchBar', UniSearchBar);
+Vue.component('UniSection', UniSection);
+Vue.component('UniSegmentedControl', UniSegmentedControl);
+Vue.component('UniSteps', UniSteps);
+Vue.component('UniSwipeAction', UniSwipeAction);
+Vue.component('UniSwipeActionItem', UniSwipeActionItem);
+Vue.component('UniSwiperDot', UniSwiperDot);
+Vue.component('UniTable', UniTable);
+Vue.component('UniTbody', UniTbody);
+Vue.component('UniTd', UniTd);
+Vue.component('UniTh', UniTh);
+Vue.component('UniThead', UniThead);
+Vue.component('UniTr', UniTr);
+Vue.component('UniTag', UniTag);
+Vue.component('UniTest', UniTest);
+Vue.component('UniTitle', UniTitle);
+Vue.component('UniTooltip', UniTooltip);
+Vue.component('UniTransition', UniTransition);
+Vue.component('UniUi', UniUi);

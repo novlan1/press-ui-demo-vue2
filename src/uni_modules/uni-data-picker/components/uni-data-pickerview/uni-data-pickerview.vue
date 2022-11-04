447a4890c6caf39033d668fc30e7e33d2edf8216
novlan1 <1576271227@qq.com>
2022-11-04 12:00:22 +0800

    feat: 解决lint问题

--- src/uni_modules/uni-data-picker/components/uni-data-pickerview/uni-data-pickerview.vue
+++ src/uni_modules/uni-data-picker/components/uni-data-pickerview/uni-data-pickerview.vue
@@ -1,41 +1,76 @@
 <template>
   <view class="uni-data-pickerview">
-    <scroll-view v-if="!isCloudDataList" class="selected-area" scroll-x="true">
+    <scroll-view
+      v-if="!isCloudDataList"
+      class="selected-area"
+      scroll-x="true"
+    >
       <view class="selected-list">
         <template v-for="(item,index) in selected">
-          <view class="selected-item"
+          <view
+            v-if="item.text"
+            :key="index"
+            class="selected-item"
             :class="{'selected-item-active':index==selectedIndex}"
-            v-if="item.text" @click="handleSelect(index)">
-            <text>{{item.text}}</text>
+            @click="handleSelect(index)"
+          >
+            <text>{{ item.text }}</text>
           </view>
         </template>
       </view>
     </scroll-view>
     <view class="tab-c">
       <template v-for="(child, i) in dataList">
-        <scroll-view class="list" :key="i" v-if="i==selectedIndex" :scroll-y="true">
-          <view class="item" :class="{'is-disabled': !!item.disable}" v-for="(item, j) in child"
-            @click="handleNodeClick(item, i, j)">
-            <text class="item-text">{{item[map.text]}}</text>
-            <view class="check" v-if="selected.length > i && item[map.value] == selected[i].value"></view>
+        <scroll-view
+          v-if="i==selectedIndex"
+          :key="i"
+          class="list"
+          :scroll-y="true"
+        >
+          <view
+            v-for="(item, j) in child"
+            :key="j"
+            class="item"
+            :class="{'is-disabled': !!item.disable}"
+            @click="handleNodeClick(item, i, j)"
+          >
+            <text class="item-text">
+              {{ item[map.text] }}
+            </text>
+            <view
+              v-if="selected.length > i && item[map.value] == selected[i].value"
+              class="check"
+            />
           </view>
         </scroll-view>
       </template>
 
-      <view class="loading-cover" v-if="loading">
-        <uni-load-more class="load-more" :contentText="loadMore" status="loading"></uni-load-more>
+      <view
+        v-if="loading"
+        class="loading-cover"
+      >
+        <uni-load-more
+          class="load-more"
+          :content-text="loadMore"
+          status="loading"
+        />
       </view>
-      <view class="error-message" v-if="errorMessage">
-        <text class="error-text">{{errorMessage}}</text>
+      <view
+        v-if="errorMessage"
+        class="error-message"
+      >
+        <text class="error-text">
+          {{ errorMessage }}
+        </text>
       </view>
     </view>
   </view>
 </template>
 
 <script>
-  import dataPicker from "./uni-data-picker.js"
+import dataPicker from './uni-data-picker.js';
 
-  /**
+/**
    * DataPickerview
    * @description uni-data-pickerview
    * @tutorial https://ext.dcloud.net.cn/plugin?id=3796
@@ -50,272 +85,272 @@
    * @property {String} orderby 排序字段及正序倒叙设置
    * @property {String|JQLString} where 查询条件
    */
-  export default {
-    name: 'UniDataPickerView',
-    emits: ['nodeclick', 'change', 'datachange', 'update:modelValue'],
-    mixins: [dataPicker],
-    props: {
-      managedMode: {
-        type: Boolean,
-        default: false
-      },
-      ellipsis: {
-        type: Boolean,
-        default: true
-      }
+export default {
+  name: 'UniDataPickerView',
+  mixins: [dataPicker],
+  props: {
+    managedMode: {
+      type: Boolean,
+      default: false,
     },
-    created() {
-      if (!this.managedMode) {
-        this.$nextTick(() => {
-          this.loadData();
-        })
-      }
+    ellipsis: {
+      type: Boolean,
+      default: true,
     },
-    methods: {
-      onPropsChange() {
-        this._treeData = [];
-        this.selectedIndex = 0;
-        this.$nextTick(() => {
-          this.loadData();
-        })
-      },
-      handleSelect(index) {
-        this.selectedIndex = index;
-      },
-      handleNodeClick(item, i, j) {
-        if (item.disable) {
-          return;
-        }
-
-        const node = this.dataList[i][j];
-        const text = node[this.map.text];
-        const value = node[this.map.value];
-
-        if (i < this.selected.length - 1) {
-          this.selected.splice(i, this.selected.length - i)
-          this.selected.push({
-            text,
-            value
-          })
-        } else if (i === this.selected.length - 1) {
-          this.selected.splice(i, 1, {
-            text,
-            value
-          })
-        }
-
-        if (node.isleaf) {
-          this.onSelectedChange(node, node.isleaf)
-          return
-        }
-
-        const {
-          isleaf,
-          hasNodes
-        } = this._updateBindData()
-
-        // 本地数据
-        if (this.isLocalData) {
-          this.onSelectedChange(node, (!hasNodes || isleaf))
-        } else if (this.isCloudDataList) { // Cloud 数据 (单列)
-          this.onSelectedChange(node, true)
-        } else if (this.isCloudDataTree) { // Cloud 数据 (树形)
-          if (isleaf) {
-            this.onSelectedChange(node, node.isleaf)
-          } else if (!hasNodes) { // 请求一次服务器以确定是否为叶子节点
-            this.loadCloudDataNode((data) => {
-              if (!data.length) {
-                node.isleaf = true
-              } else {
-                this._treeData.push(...data)
-                this._updateBindData(node)
-              }
-              this.onSelectedChange(node, node.isleaf)
-            })
-          }
-        }
-      },
-      updateData(data) {
-        this._treeData = data.treeData
-        this.selected = data.selected
-        if (!this._treeData.length) {
-          this.loadData()
-        } else {
-          //this.selected = data.selected
-          this._updateBindData()
-        }
-      },
-      onDataChange() {
-        this.$emit('datachange');
-      },
-      onSelectedChange(node, isleaf) {
-        if (isleaf) {
-          this._dispatchEvent()
-        }
-
-        if (node) {
-          this.$emit('nodeclick', node)
-        }
-      },
-      _dispatchEvent() {
-        this.$emit('change', this.selected.slice(0))
-      }
+  },
+  emits: ['nodeclick', 'change', 'datachange', 'update:modelValue'],
+  created() {
+    if (!this.managedMode) {
+      this.$nextTick(() => {
+        this.loadData();
+      });
     }
-  }
+  },
+  methods: {
+    onPropsChange() {
+      this._treeData = [];
+      this.selectedIndex = 0;
+      this.$nextTick(() => {
+        this.loadData();
+      });
+    },
+    handleSelect(index) {
+      this.selectedIndex = index;
+    },
+    handleNodeClick(item, i, j) {
+      if (item.disable) {
+        return;
+      }
+
+      const node = this.dataList[i][j];
+      const text = node[this.map.text];
+      const value = node[this.map.value];
+
+      if (i < this.selected.length - 1) {
+        this.selected.splice(i, this.selected.length - i);
+        this.selected.push({
+          text,
+          value,
+        });
+      } else if (i === this.selected.length - 1) {
+        this.selected.splice(i, 1, {
+          text,
+          value,
+        });
+      }
+
+      if (node.isleaf) {
+        this.onSelectedChange(node, node.isleaf);
+        return;
+      }
+
+      const {
+        isleaf,
+        hasNodes,
+      } = this._updateBindData();
+
+      // 本地数据
+      if (this.isLocalData) {
+        this.onSelectedChange(node, (!hasNodes || isleaf));
+      } else if (this.isCloudDataList) { // Cloud 数据 (单列)
+        this.onSelectedChange(node, true);
+      } else if (this.isCloudDataTree) { // Cloud 数据 (树形)
+        if (isleaf) {
+          this.onSelectedChange(node, node.isleaf);
+        } else if (!hasNodes) { // 请求一次服务器以确定是否为叶子节点
+          this.loadCloudDataNode((data) => {
+            if (!data.length) {
+              node.isleaf = true;
+            } else {
+              this._treeData.push(...data);
+              this._updateBindData(node);
+            }
+            this.onSelectedChange(node, node.isleaf);
+          });
+        }
+      }
+    },
+    updateData(data) {
+      this._treeData = data.treeData;
+      this.selected = data.selected;
+      if (!this._treeData.length) {
+        this.loadData();
+      } else {
+        // this.selected = data.selected
+        this._updateBindData();
+      }
+    },
+    onDataChange() {
+      this.$emit('datachange');
+    },
+    onSelectedChange(node, isleaf) {
+      if (isleaf) {
+        this._dispatchEvent();
+      }
+
+      if (node) {
+        this.$emit('nodeclick', node);
+      }
+    },
+    _dispatchEvent() {
+      this.$emit('change', this.selected.slice(0));
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-	$uni-primary: #007aff !default;
+$uni-primary: #007aff !default;
 
-	.uni-data-pickerview {
-		flex: 1;
-		/* #ifndef APP-NVUE */
-		display: flex;
-		/* #endif */
-		flex-direction: column;
-		overflow: hidden;
-		height: 100%;
-	}
-
-  .error-text {
-    color: #DD524D;
-  }
-
-  .loading-cover {
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(255, 255, 255, .5);
-    /* #ifndef APP-NVUE */
-    display: flex;
-    /* #endif */
-    flex-direction: column;
-    align-items: center;
-    z-index: 1001;
-  }
-
-  .load-more {
-    /* #ifndef APP-NVUE */
-    margin: auto;
-    /* #endif */
-  }
-
-  .error-message {
-    background-color: #fff;
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    padding: 15px;
-    opacity: .9;
-    z-index: 102;
-  }
-
-  /* #ifdef APP-NVUE */
-  .selected-area {
-    width: 750rpx;
-  }
+.uni-data-pickerview {
+  flex: 1;
+  /* #ifndef APP-NVUE */
+  display: flex;
   /* #endif */
+  flex-direction: column;
+  overflow: hidden;
+  height: 100%;
+}
 
-  .selected-list {
-    /* #ifndef APP-NVUE */
-    display: flex;
-    flex-wrap: nowrap;
-    /* #endif */
-    flex-direction: row;
-    padding: 0 5px;
-    border-bottom: 1px solid #f8f8f8;
-  }
+.error-text {
+  color: #dd524d;
+}
 
-  .selected-item {
-    margin-left: 10px;
-    margin-right: 10px;
-    padding: 12px 0;
-    text-align: center;
-    /* #ifndef APP-NVUE */
-    white-space: nowrap;
-    /* #endif */
-  }
+.loading-cover {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.5);
+  /* #ifndef APP-NVUE */
+  display: flex;
+  /* #endif */
+  flex-direction: column;
+  align-items: center;
+  z-index: 1001;
+}
 
-  .selected-item-text-overflow {
-    width: 168px;
-    /* fix nvue */
-    overflow: hidden;
-    /* #ifndef APP-NVUE */
-    width: 6em;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    -o-text-overflow: ellipsis;
-    /* #endif */
-  }
+.load-more {
+  /* #ifndef APP-NVUE */
+  margin: auto;
+  /* #endif */
+}
 
-	.selected-item-active {
-		border-bottom: 2px solid $uni-primary;
-	}
+.error-message {
+  background-color: #fff;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 15px;
+  opacity: 0.9;
+  z-index: 102;
+}
 
-	.selected-item-text {
-		color: $uni-primary;
-	}
+/* #ifdef APP-NVUE */
+.selected-area {
+  width: 750rpx;
+}
+/* #endif */
 
-  .tab-c {
-    position: relative;
-    flex: 1;
-    /* #ifndef APP-NVUE */
-    display: flex;
-    /* #endif */
-    flex-direction: row;
-    overflow: hidden;
-  }
+.selected-list {
+  /* #ifndef APP-NVUE */
+  display: flex;
+  flex-wrap: nowrap;
+  /* #endif */
+  flex-direction: row;
+  padding: 0 5px;
+  border-bottom: 1px solid #f8f8f8;
+}
 
-  .list {
-    flex: 1;
-  }
+.selected-item {
+  margin-left: 10px;
+  margin-right: 10px;
+  padding: 12px 0;
+  text-align: center;
+  /* #ifndef APP-NVUE */
+  white-space: nowrap;
+  /* #endif */
+}
 
-  .item {
-    padding: 12px 15px;
-    /* border-bottom: 1px solid #f0f0f0; */
-    /* #ifndef APP-NVUE */
-    display: flex;
-    /* #endif */
-    flex-direction: row;
-    justify-content: space-between;
-  }
+.selected-item-text-overflow {
+  width: 168px;
+  /* fix nvue */
+  overflow: hidden;
+  /* #ifndef APP-NVUE */
+  width: 6em;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  -o-text-overflow: ellipsis;
+  /* #endif */
+}
 
-  .is-disabled {
-    opacity: .5;
-  }
+.selected-item-active {
+  border-bottom: 2px solid $uni-primary;
+}
 
-  .item-text {
-    /* flex: 1; */
-    color: #333333;
-  }
+.selected-item-text {
+  color: $uni-primary;
+}
 
-  .item-text-overflow {
-    width: 280px;
-    /* fix nvue */
-    overflow: hidden;
-    /* #ifndef APP-NVUE */
-    width: 20em;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    -o-text-overflow: ellipsis;
-    /* #endif */
-  }
+.tab-c {
+  position: relative;
+  flex: 1;
+  /* #ifndef APP-NVUE */
+  display: flex;
+  /* #endif */
+  flex-direction: row;
+  overflow: hidden;
+}
 
-	.check {
-		margin-right: 5px;
-		border: 2px solid $uni-primary;
-		border-left: 0;
-		border-top: 0;
-		height: 12px;
-		width: 6px;
-		transform-origin: center;
-		/* #ifndef APP-NVUE */
-		transition: all 0.3s;
-		/* #endif */
-		transform: rotate(45deg);
-	}
+.list {
+  flex: 1;
+}
+
+.item {
+  padding: 12px 15px;
+  /* border-bottom: 1px solid #f0f0f0; */
+  /* #ifndef APP-NVUE */
+  display: flex;
+  /* #endif */
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.is-disabled {
+  opacity: 0.5;
+}
+
+.item-text {
+  /* flex: 1; */
+  color: #333333;
+}
+
+.item-text-overflow {
+  width: 280px;
+  /* fix nvue */
+  overflow: hidden;
+  /* #ifndef APP-NVUE */
+  width: 20em;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  -o-text-overflow: ellipsis;
+  /* #endif */
+}
+
+.check {
+  margin-right: 5px;
+  border: 2px solid $uni-primary;
+  border-left: 0;
+  border-top: 0;
+  height: 12px;
+  width: 6px;
+  transform-origin: center;
+  /* #ifndef APP-NVUE */
+  transition: all 0.3s;
+  /* #endif */
+  transform: rotate(45deg);
+}
 </style>

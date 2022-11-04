e11a7b7692570f717559dca63d26ba39428ac491
novlan1 <1576271227@qq.com>
2022-11-04 11:59:09 +0800

    feat: 增加picker

---
+++ src/pages/match/picker/picker.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>
+    <match-picker
+      :show="show"
+      :columns="columns"
+    />
+    <!-- <u-button @click="show = true">打开</u-button> -->
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+      columns: [
+        ['中国', '美国', '日本'],
+      ],
+    };
+  },
+};
+</script>

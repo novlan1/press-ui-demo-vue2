6d410a7ad2c900600b1227cd894ab9ada0b5d943
novlan1 <1576271227@qq.com>
2022-11-05 13:30:16 +0800

    fix: popup找不到parent

--- src/uni_modules/uni-popup/components/uni-popup-message/uni-popup-message.vue
+++ src/uni_modules/uni-popup/components/uni-popup-message/uni-popup-message.vue
@@ -1,16 +1,24 @@
 <template>
-	<view class="uni-popup-message">
-		<view class="uni-popup-message__box fixforpc-width" :class="'uni-popup__'+type">
-			<slot>
-				<text class="uni-popup-message-text" :class="'uni-popup__'+type+'-text'">{{message}}</text>
-			</slot>
-		</view>
-	</view>
+  <view class="uni-popup-message">
+    <view
+      class="uni-popup-message__box fixforpc-width"
+      :class="'uni-popup__'+type"
+    >
+      <slot>
+        <text
+          class="uni-popup-message-text"
+          :class="'uni-popup__'+type+'-text'"
+        >
+          {{ message }}
+        </text>
+      </slot>
+    </view>
+  </view>
 </template>
 
 <script>
-	import popup from '../uni-popup/popup.js'
-	/**
+import popup from '../uni-popup/popup.js';
+/**
 	 * PopUp 弹出层-消息提示
 	 * @description 弹出层-消息提示
 	 * @tutorial https://ext.dcloud.net.cn/plugin?id=329
@@ -23,121 +31,121 @@
 	 * @property {String} duration 显示时间，设置为 0 则不会自动关闭
 	 */
 
-	export default {
-		name: 'uniPopupMessage',
-		mixins:[popup],
-		props: {
-			/**
+export default {
+  name: 'UniPopupMessage',
+  mixins: [popup],
+  props: {
+    /**
 			 * 主题 success/warning/info/error	  默认 success
 			 */
-			type: {
-				type: String,
-				default: 'success'
-			},
-			/**
+    type: {
+      type: String,
+      default: 'success',
+    },
+    /**
 			 * 消息文字
 			 */
-			message: {
-				type: String,
-				default: ''
-			},
-			/**
+    message: {
+      type: String,
+      default: '',
+    },
+    /**
 			 * 显示时间，设置为 0 则不会自动关闭
 			 */
-			duration: {
-				type: Number,
-				default: 3000
-			},
-			maskShow:{
-				type:Boolean,
-				default:false
-			}
-		},
-		data() {
-			return {}
-		},
-		created() {
-			this.popup.maskShow = this.maskShow
-			this.popup.messageChild = this
-		},
-		methods: {
-			timerClose(){
-				if(this.duration === 0) return
-				clearTimeout(this.timer) 
-				this.timer = setTimeout(()=>{
-					this.popup.close()
-				},this.duration)
-			}
-		}
-	}
+    duration: {
+      type: Number,
+      default: 3000,
+    },
+    maskShow: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {};
+  },
+  created() {
+    this.popup.maskShow = this.maskShow;
+    this.popup.messageChild = this;
+  },
+  methods: {
+    timerClose() {
+      if (this.duration === 0) return;
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        this.popup.close();
+      }, this.duration);
+    },
+  },
+};
 </script>
-<style lang="scss" >
-	.uni-popup-message {
-		/* #ifndef APP-NVUE */
-		display: flex;
-		/* #endif */
-		flex-direction: row;
-		justify-content: center;
-	}
+<style lang="scss">
+.uni-popup-message {
+  /* #ifndef APP-NVUE */
+  display: flex;
+  /* #endif */
+  flex-direction: row;
+  justify-content: center;
+}
 
-	.uni-popup-message__box {
-		background-color: #e1f3d8;
-		padding: 10px 15px;
-		border-color: #eee;
-		border-style: solid;
-		border-width: 1px;
-		flex: 1;
-	}
+.uni-popup-message__box {
+  background-color: #e1f3d8;
+  padding: 10px 15px;
+  border-color: #eee;
+  border-style: solid;
+  border-width: 1px;
+  flex: 1;
+}
 
-	@media screen and (min-width: 500px) {
-		.fixforpc-width {
-			margin-top: 20px;
-			border-radius: 4px;
-			flex: none;
-			min-width: 380px;
-			/* #ifndef APP-NVUE */
-			max-width: 50%;
-			/* #endif */
-			/* #ifdef APP-NVUE */
-			max-width: 500px;
-			/* #endif */
-		}
-	}
+@media screen and (min-width: 500px) {
+  .fixforpc-width {
+    margin-top: 20px;
+    border-radius: 4px;
+    flex: none;
+    min-width: 380px;
+    /* #ifndef APP-NVUE */
+    max-width: 50%;
+    /* #endif */
+    /* #ifdef APP-NVUE */
+    max-width: 500px;
+    /* #endif */
+  }
+}
 
-	.uni-popup-message-text {
-		font-size: 14px;
-		padding: 0;
-	}
+.uni-popup-message-text {
+  font-size: 14px;
+  padding: 0;
+}
 
-	.uni-popup__success {
-		background-color: #e1f3d8;
-	}
+.uni-popup__success {
+  background-color: #e1f3d8;
+}
 
-	.uni-popup__success-text {
-		color: #67C23A;
-	}
+.uni-popup__success-text {
+  color: #67c23a;
+}
 
-	.uni-popup__warn {
-		background-color: #faecd8;
-	}
+.uni-popup__warn {
+  background-color: #faecd8;
+}
 
-	.uni-popup__warn-text {
-		color: #E6A23C;
-	}
+.uni-popup__warn-text {
+  color: #e6a23c;
+}
 
-	.uni-popup__error {
-		background-color: #fde2e2;
-	}
+.uni-popup__error {
+  background-color: #fde2e2;
+}
 
-	.uni-popup__error-text {
-		color: #F56C6C;
-	}
+.uni-popup__error-text {
+  color: #f56c6c;
+}
 
-	.uni-popup__info {
-		background-color: #F2F6FC;
-	}
+.uni-popup__info {
+  background-color: #f2f6fc;
+}
 
-	.uni-popup__info-text {
-		color: #909399;
-	}
+.uni-popup__info-text {
+  color: #909399;
+}
 </style>
--- src/uni_modules/uni-popup/components/uni-popup/popup.js
+++ src/uni_modules/uni-popup/components/uni-popup/popup.js
@@ -1,26 +1,28 @@
 
 export default {
-	data() {
-		return {
-			
-		}
-	},
-	created(){
-		this.popup = this.getParent()
-	},
-	methods:{
-		/**
+  data() {
+    return {
+
+    };
+  },
+  created() {
+    this.popup = this.getParent();
+  },
+  methods: {
+    /**
 		 * 获取父元素实例
 		 */
-		getParent(name = 'uniPopup') {
-			let parent = this.$parent;
-			let parentName = parent.$options.name;
-			while (parentName !== name) {
-				parent = parent.$parent;
-				if (!parent) return false
-				parentName = parent.$options.name;
-			}
-			return parent;
-		},
-	}
-}
+    getParent(name = 'UniPopup') {
+      let parent = this.$parent;
+      let parentName = parent.$options.name;
+      while (parentName !== name) {
+        console.log('parent', parent, parentName);
+        parent = parent.$parent;
+        if (!parent) return false;
+        parentName = parent.$options.name;
+      }
+      console.log('res', parent);
+      return parent;
+    },
+  },
+};

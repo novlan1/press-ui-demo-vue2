b07c88eb0597c17db8f6344e2845365e2e803762
guowangyang <guowangyang@tencent.com>
2022-12-11 22:04:31 +0800

    feat(rate): 增加rate

---
+++ src/packages/press-rate/demo.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="wrap">
+    <demo-block title="基础用法">
+      <press-rate
+        :value="3"
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="自定义图标">
+      <press-rate
+        :value="value"
+        icon="like"
+        void-icon="like-o"
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="自定义样式">
+      <press-rate
+        :value="value"
+        :size="25"
+        color="#ffd21e"
+        void-icon="star"
+        void-color="#eee"
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="半星">
+      <press-rate
+        :value="half"
+        allow-half
+        void-icon="star"
+        void-color="#eee"
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="自定义数量">
+      <press-rate
+        :value="value"
+        :count="6"
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="禁用状态">
+      <press-rate
+        :value="value"
+        disabled
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="只读状态">
+      <press-rate
+        :value="value"
+        readonly
+        @change="onChange"
+      />
+    </demo-block>
+  </div>
+</template>
+<script>
+import PressRate from 'src/packages/press-rate/press-rate.vue';
+export default {
+  components: {
+    PressRate,
+  },
+  data() {
+    return {
+      value: 3,
+      half: 2.5,
+    };
+  },
+  methods: {
+    onChange(val) {
+      console.log('onChange.val', val);
+    },
+
+  },
+};
+</script>
+<style scoped lang="scss">
+@import "src/packages/base/mixin.scss";
+</style>

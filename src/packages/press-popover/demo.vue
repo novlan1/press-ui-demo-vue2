88eb1dae7bc402bdc5a60dc0caebd5ad4d3f9f18
guowangyang <guowangyang@tencent.com>
2022-12-15 11:24:04 +0800

    feat(popover): 增加popover

---
+++ src/packages/press-popover/demo.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="demo-wrap">
+    <div class="popover-wrap">
+      <PressPopover
+        :show="true"
+        placement="bottom"
+        custom-class="demo-nav-popover demo-nav-popover--bottom"
+      >
+        <div
+          v-for="(item,index) of list"
+          :key="index"
+          class="demo-nav-item"
+        >
+          选项{{ item }}
+        </div>
+        <div
+          class="demo-nav-item"
+        >
+          bottom
+        </div>
+      </PressPopover>
+
+      <PressPopover
+        :show="true"
+        placement="top"
+        custom-class="demo-nav-popover demo-nav-popover--top"
+      >
+        <div
+          v-for="(item,index) of list"
+          :key="index"
+          class="demo-nav-item"
+        >
+          选项{{ item }}
+        </div>
+        <div
+          class="demo-nav-item"
+        >
+          top
+        </div>
+      </PressPopover>
+
+      <PressPopover
+        :show="true"
+        placement="right"
+        custom-class="demo-nav-popover demo-nav-popover--right"
+      >
+        <div
+          v-for="(item,index) of list"
+          :key="index"
+          class="demo-nav-item"
+        >
+          选项{{ item }}
+        </div>
+        <div
+          class="demo-nav-item"
+        >
+          right
+        </div>
+      </PressPopover>
+
+      <PressPopover
+        :show="true"
+        placement="left"
+        custom-class="demo-nav-popover demo-nav-popover--left"
+      >
+        <div
+          v-for="(item,index) of list"
+          :key="index"
+          class="demo-nav-item"
+        >
+          选项{{ item }}
+        </div>
+        <div
+          class="demo-nav-item"
+        >
+          left
+        </div>
+      </PressPopover>
+    </div>
+  </div>
+</template>
+<script>
+import PressPopover from 'src/packages/press-popover/press-popover.vue';
+
+
+export default {
+  components: {
+    PressPopover,
+  },
+  data() {
+    return {
+      list: [1, 2, 3],
+
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+<style scoped lang="scss">
+@import "src/packages/base/mixin.scss";
+$GAP: 56px;
+
+.popover-wrap {
+  position: relative;
+  width: 100%;
+  height: 10px;
+
+  .demo-nav-popover {
+    top: 30px;
+    bottom: auto;
+    padding: 0;
+    background: #09134e;
+    &--left {
+      left: auto;
+      right: $GAP;
+      top: 238px;
+    }
+
+    &--right {
+      left: $GAP;
+      right: auto;
+      top: 238px;
+    }
+
+    &--top {
+      left: $GAP;
+      right: auto;
+    }
+    &--bottom {
+      left: auto;
+      right: $GAP;
+    }
+  }
+  .demo-nav-item {
+    display: flex;
+    align-items: center;
+    padding: 0 22px;
+    height: 40px;
+    color: #fff;
+  }
+}
+</style>
---
+++ src/packages/press-popover/press-popover.vue
@@ -0,0 +1,150 @@
+<template>
+  <div
+    v-if="innerShow"
+    :class="['press-popover',
+             `press-popover--${placement}`,
+             popperClass,
+             customClass,
+             animateShow ? 'tip-match-fade-in':'tip-match-fade-out'
+    ]"
+  >
+    <slot />
+  </div>
+</template>
+<script>
+import { defaultProps, defaultOptions } from '../common/press-component';
+
+export default {
+  options: {
+    ...defaultOptions,
+    virtualHost: true,
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: true,
+    },
+    placement: {
+      type: String,
+      default: 'right',
+    },
+    popperClass: {
+      type: String,
+      default: '',
+    },
+    ...defaultProps,
+  },
+
+  data() {
+    return {
+      innerShow: false,
+      animateShow: false,
+    };
+  },
+  computed: {
+  },
+  watch: {
+    show: {
+      handler(newVal) {
+        if (newVal) {
+          this.innerShow = newVal;
+          this.animateShow = newVal;
+        } else {
+          // remove
+          this.animateShow = newVal;
+          setTimeout(() => {
+            this.innerShow = newVal;
+          }, 300);
+        }
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+  },
+};
+
+</script>
+<style lang="scss" scoped>
+@import "../base/layout.scss";
+
+.press-popover {
+  position: absolute;
+  font-size: tovmin(24);
+  color: $color-white;
+  padding: tovmin(8) tovmin(24);
+  border-radius: tovmin(8);
+  background: rgba(0, 0, 37, 0.95);
+  z-index: 8;
+
+  &::before {
+    position: absolute;
+    content: "";
+    width: 0;
+    height: 0;
+    border-left: tovmin(12) solid transparent;
+    border-right: tovmin(12) solid transparent;
+    border-top: tovmin(12) solid rgba(0, 0, 37, 0.95);
+  }
+
+  &--top {
+    top: -100%;
+    left: 0;
+    right: 0;
+    margin: auto;
+
+    &::before {
+      bottom: tovmin(-12);
+      left: 0;
+      right: 0;
+      margin: auto;
+      transform: rotate(0deg);
+    }
+  }
+
+  &--right {
+    top: 0;
+    bottom: 0;
+    right: -100%;
+    margin: auto;
+
+    &::before {
+      top: 0;
+      bottom: 0;
+      left: tovmin(-18);
+      margin: auto;
+      transform: rotate(90deg);
+    }
+  }
+
+  &--bottom {
+    bottom: -100%;
+    left: 0;
+    right: 0;
+    margin: auto;
+
+    &::before {
+      top: tovmin(-12);
+      left: 0;
+      right: 0;
+      margin: auto;
+      transform: rotate(180deg);
+    }
+  }
+
+  &--left {
+    bottom: 0;
+    left: -100%;
+    top: 0;
+    margin: auto;
+
+    &::before {
+      top: 0;
+      bottom: 0;
+      right: tovmin(-18);
+      margin: auto;
+      transform: rotate(-90deg);
+    }
+  }
+}
+</style>

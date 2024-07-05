e8e3341fb6693089fdbfe2e4ba47bbd5dc5f3f0d
novlan1 <1576271227@qq.com>
2024-07-05 17:10:51 +0800

    chore: update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,51 @@
 const path = require('path');
+const {
+  RemToRpxPlugin,
+  GenVersionMpPlugin,
+  GenVersionWebPlugin,
+  DispatchVuePlugin,
+  DispatchScriptPlugin,
+  FixNpmPackagePlugin,
+} = require('plugin-light/lib/plugin');
+const { BUILD_NAME_MAP } = require('t-comm/lib/v-console/config');
+
+
+const plugins = [];
+
+if (process.env.UNI_PLATFORM !== 'h5') {
+  plugins.push(new GenVersionMpPlugin());
+  plugins.push(new RemToRpxPlugin({}));
+
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push(new DispatchVuePlugin({
+      insertRequireVendor: true,
+      moveComponents: {
+        disableList: ['@zebra-ui'],
+      },
+    }));
+    plugins.push(new DispatchScriptPlugin({
+      addCommonVendorRequire: true,
+      whiteList: [],
+    }));
+    plugins.push(new FixNpmPackagePlugin());
+  }
+} else {
+  plugins.push(new GenVersionWebPlugin({
+    buildName: BUILD_NAME_MAP.build,
+    commitName: BUILD_NAME_MAP.commit,
+    delay: 1,
+  }));
+}
+
 
 module.exports = {
   configureWebpack: {
-    // devServer: {
-    //   port: 9999,
-    //   // 防止报错：Invalid Host header
-    //   disableHostCheck: true,
-    // },
     resolve: {
       alias: {
         src: path.resolve(__dirname, './src'),
-        // 'press-ui': path.resolve(__dirname, './src/packages/'),
       },
     },
-    // plugins,
+    plugins,
   },
   transpileDependencies: ['press-ui', '@zebra-ui/swiper'],
 };

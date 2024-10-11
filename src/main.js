22fa77b123b6b74e0ca7911be45fc3517ea0cb7d
novlan1 <1576271227@qq.com>
2024-10-11 16:53:37 +0800

    feat: 更新引入位置

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ import './uni.promisify.adaptor';
 import { initMixin } from './utils/mixin/mixin';
 import { checkAndShowVConsole } from 't-comm/lib/v-console/toggle';
 
-import DemoBlock from './packages/press-demo-block/press-demo-block.vue';
-import ToggleHeader from './packages/press-toggle-header/press-toggle-header.vue';
-import { initDemoI18n } from './packages/locale/demo-lang';
+import DemoBlock from 'press-ui/press-demo-block/press-demo-block.vue';
+import ToggleHeader from 'press-ui/press-toggle-header/press-toggle-header.vue';
+import { initDemoI18n } from 'press-ui/locale/demo-lang';
 
-import PressCell from './packages/press-cell/press-cell.vue';
-import PressPicker from './packages/press-picker/press-picker.vue';
-import PressPopupCell from './packages/press-popup-cell/press-popup-cell.vue';
-import './packages/common/touch-emulator/index';
+import PressCell from 'press-ui/press-cell/press-cell.vue';
+import PressPicker from 'press-ui/press-picker/press-picker.vue';
+import PressPopupCell from 'press-ui/press-popup-cell/press-popup-cell.vue';
+import 'press-ui/common/touch-emulator/index';
 
 Vue.config.productionTip = false;
 
